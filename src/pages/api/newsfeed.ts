6c987761a1037e5d457dc0f73e02a8936a981af5
Sahas Nanabala <sahas@stockinsights.ai>
2023-06-25 15:38:22 +0530

    Added limit

--- src/pages/api/newsfeed.ts
+++ src/pages/api/newsfeed.ts
@@ -38,7 +38,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
     console.log("Query filter", query)
 
-    const newsFeeds = await NewsFeed.find(query);
+    const newsFeeds = await NewsFeed.find(query).limit(process.env.FEED_LIMIT ? Number(process.env.FEED_LIMIT as string): 500);
 
     return res.status(200).json({
       message: 'Success',
